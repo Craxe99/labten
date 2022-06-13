37491138145d3de0b64809962284e817b51c3520
Kirill Varyukhin <craxe@yandex.ru>
2022-06-13 12:38:10 +0300

    first commit

---
+++ components/about.php
@@ -0,0 +1,10 @@
+<section class="about container">
+  <h2 class="section-title">Привет, я <?= $name ?></h2>
+  <img class="about-image" src="<?= $image ?>" width="147" height="147" alt="<?= $name ?>">
+  <div class="about-content">
+  <p>Анекдот.</p>
+  <p>Встретились как-то двое умственно отсталых в больнице.</p>
+  <p>Первый говорит: "Только не надо писать фронтенд на php"</p>
+  <p>А второй отвечает: "Ну вы же бэкэенд на js пишете!"</p>
+  </div>
+</section>
---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+$title = 'PHP + HTML, или инструкция как НЕ нужно создавать сайты.';
+$name = 'Архимаг Пыхи - худшего "языка" программирования';
+$image = 'img/vor.jpg';
+$email = 'kaktebetakoeelonmask.com';
+$phone = '+2139128491204194091240192049120490124092104910249012049104910490';
+
+require('components/header.php');
+require('components/status.php');
+require('components/about.php');
+require('components/photos.php');
+require('components/news.php');
+require('components/quotes.php');
+require('components/contacts.php');
+require('components/footer.php');
